ff9ea576fcbd9fdbcf45b39ffa33ee726eab584e
Norhannageh123 <84436814+Norhannageh123@users.noreply.github.com>
2021-05-19 13:24:38 +0200

    Add files via upload

---
+++ task.2(3).c
@@ -0,0 +1,32 @@
+/*Table of squares and cubes*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n=0;
+    printf("number square  cube\n");
+    printf("%d%9d%9d\n",n,n*n,n*n*n);
+    n++;
+    printf("%d%9d%9d\n",n,n*n,n*n*n);
+    n++;
+    printf("%d%9d%9d\n",n,n*n,n*n*n);
+    n++;
+    printf("%d%9d%9d\n",n,n*n,n*n*n);
+    n++;
+    printf("%d%9d%9d\n",n,n*n,n*n*n);
+    n++;
+    printf("%d%9d%9d\n",n,n*n,n*n*n);
+    n++;
+    printf("%d%9d%9d\n",n,n*n,n*n*n);
+    n++;
+    printf("%d%9d%9d\n",n,n*n,n*n*n);
+    n++;
+    printf("%d%9d%9d\n",n,n*n,n*n*n);
+    n++;
+    printf("%d%9d%9d\n",n,n*n,n*n*n);
+    n++;
+    printf("%d%9d%9d\n",n,n*n,n*n*n);
+    n++;
+    return 0;
+}
