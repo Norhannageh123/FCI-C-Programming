ff59d6002bd03b4717cb36fcd57b8e5bb307ad19
Norhannageh123 <84436814+Norhannageh123@users.noreply.github.com>
2021-05-29 20:30:05 +0200

    Add files via upload

---
+++ calculating credit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int account1.account2,account3,clbr1,clbr2,clbr3;
+    scanf("%d%d%d%d%d%d",account1,account2,account3,clbr1,clbr2,clbr3);
+    printf("%d",clbr1/2,account1);
+    printf("%d",clbr2/2,account2);
+    printf("%d",clbr3/2,account3);
+    return 0;
+}
