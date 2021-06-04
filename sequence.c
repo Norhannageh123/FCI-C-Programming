3534ed28988dff6ae4896cdeea2f55069330843e
Norhannageh123 <84436814+Norhannageh123@users.noreply.github.com>
2021-06-04 18:55:11 +0200

    Add files via upload

---
+++ sequence.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ for(int i=3;i<1000;i+=5){
+    printf("%d\n",i);
+ }
+ for(int n=20;n>=-1000;n-=6){
+  printf("%d\n",n);
+ }
+ for(int m=3;m<234376;m*=5){
+    printf("%d\n",m);
+ }
+ for(int x=1;x<8192;x*=2){
+    printf("%d",x);
+ }
+}
